d738521e06ebf46f1697d03d7378f07fb68d8c63
Roman Tsvetkov <tsvetkovv22@gmail.com>
2018-10-06 17:35:31 +1000

    added server and api

---
+++ src/redux/reducers/loading.js
@@ -0,0 +1,15 @@
+import { REQUEST_TODOS, GET_TODOS } from "../actions";
+
+const reducer = (state = false, action) => {
+  switch (action.type) {
+    case REQUEST_TODOS:
+      return true;
+
+    case GET_TODOS:
+      return false;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
